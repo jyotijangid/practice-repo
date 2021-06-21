a5c6185beb0c5eeb2bddc835a0ef6209a27ebf16
Jyoti Jangid <jyotik.cse17@nituk.ac.in>
2021-06-21 11:40:32 +0530

    my first commit

---
+++ sample-java-project1/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sapient.trg.pl;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		System.out.println("Thank You");
+
+	}
+
+}
